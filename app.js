7535e84578b18b740df884ba5a4e8d854847ab8c
creindle <creindle@hawaii.edu>
2017-06-19 14:58:04 -1000

    G2g

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 function fortuneTeller(){
-	console.log("Hello"); 
+	var numOfChildren = 3; 
+	var partnersName = "BESTPERSONEVER";
+	var geographicalLocation = "Rich Part of Oahu";
+	var jobTitle = "3D Game Developer";
+
+	console.log("You will be a X in Y, and married to Z with N kids.");
+
+	console.log("You will be a " + jobTitle + " in " + geographicalLocation + " , and married to " + partnersName + " with " + numOfChildren + " kids.");
 }
 
+fortuneTeller();
 function ageCalculator(){
 
 }
